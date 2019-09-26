747319d5670641436d816f0838549c0ab48059a2
Khizar723 <Khizar723@users.noreply.github.com>
2019-09-25 20:59:23 -0400

    Homework 6

---
+++ Homework 6/app.js
@@ -0,0 +1,35 @@
+const num1 = 100
+const num2 = 50
+let val
+
+//Simple Math with Numbers
+
+val = num1 + num2
+val = num1 * num2
+val = num1 - num2
+val = num1 / num2
+val = num1 % num2
+
+
+//Math Object
+
+val = Math.PI;
+val = Math.E
+val = Math.round(2.887)
+val = Math.ceil(3.412)
+val = Math.floor(3.412)
+val = Math.sqrt(100)
+val = Math.abs(-20)
+val = Math.pow(8, 2)
+val = Math.min(3,232,4343,-98,-1000)
+val = Math.max(3,232,4343,-98,-1000)
+val = Math.random()
+
+val = Math.floor(Math.random() * 20 + 1)
+
+
+
+
+
+
+console.log(val)
